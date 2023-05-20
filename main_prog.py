adb6ee6d2dc5fb7b3349c8e8c31a715295d3a4dc
miku1001 <pauloted.22@gmail.com>
2023-05-20 21:24:52 +0800

    Input first and second number

--- main_prog.py
+++ main_prog.py
@@ -1,5 +1,7 @@
 # import pyfiglet
 import pyfiglet
+# import class UserInteract
+from ui import UserInteract
 
 # Display operation Menu
 print(pyfiglet.figlet_format("Simple Mini Calculator".center(50), font = "digital"))
@@ -7,3 +9,10 @@ print("To choose your desire operation: ")
 print("\033[31mPress 1 \033[0m: Addition", " " * 35, "\033[32mPress 2 \033[0m: Subtraction \n"
       "\033[33mPress 3 \033[0m: Multiplication ", " " * 28, "\033[34mPress 4 \033[0m: Division")
 print ("=" * 90)
+
+# Assign variable for class
+ui = UserInteract()
+
+# Get the first and second numbers
+num1 = ui.input_number()
+num2 = ui.input_number()
--- ui.py
+++ ui.py
@@ -1,5 +1,9 @@
 # Create class
-    # Create method for 
+class UserInteract:
     # Create method to input number
+    def input_number(self):
+        num = float(input("Input a number: "))
+        return num
+
     # Create method to print number
     # Create method to ask user to continue or stop the program
