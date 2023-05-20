dc4234e3959b0bd089ad7ab6bf45d0a60a7c5a86
miku1001 <pauloted.22@gmail.com>
2023-05-20 21:07:24 +0800

    Pseudocodes

---
+++ calculator.py
@@ -0,0 +1,9 @@
+# Create class
+# Method for addition
+    # add num1, num2
+# Method for subtraction
+    # subtract num1, num2
+# Method for multiplication
+    # multiply num1, num2
+# Method for division
+    # divide num1, num2
