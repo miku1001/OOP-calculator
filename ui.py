6ad81b1e4c189407f1b53be2c80991cb63516c77
miku1001 <pauloted.22@gmail.com>
2023-06-03 14:48:58 +0800

    Inheritance

--- ui.py
+++ ui.py
@@ -5,10 +5,9 @@ import pyfiglet
 class UserInterface:
     # Display Operations
     def operation(self):
-        print(pyfiglet.figlet_format("Simple Mini Calculator".center(50), font = "digital"))
+        print(pyfiglet.figlet_format("                            Simple Mini Calculator".center(50), font = "digital"))
         print("To choose your desire operation: ")
-        print("\033[31mPress 1 \033[0m: Addition", " " * 35, "\033[32mPress 2 \033[0m: Subtraction \n"
-        "\033[33mPress 3 \033[0m: Multiplication ", " " * 28, "\033[34mPress 4 \033[0m: Division")
+        print("                                                             \033[31mPress 1 \033[0m: Addition", " " * 35, "\033[32mPress 2 \033[0m: Subtraction \n" "                                                             \033[33mPress 3 \033[0m: Multiplication ", " " * 28, "\033[34mPress 4 \033[0m: Division")
         print ("=" * 90)
 
     # Create method to input number
